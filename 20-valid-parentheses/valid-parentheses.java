868e20fa9e031fb19c5919f320c6f56686fadd1b
Pavel <74764952+Paveljolak@users.noreply.github.com>
2024-03-13 22:07:47 +0100

    Time: 1 ms (98.69%) | Memory: 41.1 MB (94.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for( char c : s.toCharArray()){
+            if (c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if (stack.isEmpty()){
+                    return false;
+                }
+           
+                
+            char topChar = stack.pop();
+
+            if((c == ')' && topChar != '(') ||
+               (c == '}' && topChar != '{') ||
+               (c == ']' && topChar != '[')) {
+                return false;
+            }
+              
+            }
+
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+    
+
+
