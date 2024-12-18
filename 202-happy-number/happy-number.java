e0b10dbe0c545a48ac13b2afbc3e70ddf37028bd
Pavel <74764952+Paveljolak@users.noreply.github.com>
2024-12-18 22:50:22 +0100

    Time: 1 ms (72.93%) | Memory: 40.1 MB (98.01%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -3,24 +3,21 @@ class Solution {
         int slow = n;
         int fast = n;
 
+        // Trick used here is the linked list cycle detection.
         do{
             slow = findSquare(slow);
             fast = findSquare(findSquare(fast));
-
         }while(fast != slow);
-
 
         if(slow == 1){
             return true;
         }else{
             return false;
         }
-        
     }
 
     private int findSquare(int number){
         int ans = 0;
-
         while(number > 0){
             int rem = number % 10;
             ans += rem * rem; // square 
